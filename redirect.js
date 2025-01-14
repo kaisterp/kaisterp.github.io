056418c82bd85ba7bd700dfb768673cfd113f9e7
Kai-Peters <kai2peters@gmail.com>
2025-01-14 15:54:33 +0200

    remove if statement

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-if (window.location.href==="https://kaisterp.github.io") {
-    alert("Hello World!");
-    window.location.href = "https://amazon.com"; 
-}
+// if (window.location.href==="https://kaisterp.github.io") {
+alert("Hello World!");
+window.location.href = "https://amazon.com"; 
+// }
